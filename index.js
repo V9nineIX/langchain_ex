77eced04293708084ab5f1db38a5e059bb918fe7
v9nineix <v9nineix@gmail.com>
2023-04-24 00:43:40 +0700

    Add function load doc.

--- index.js
+++ index.js
@@ -6,13 +6,23 @@
   import * as dotenv from "dotenv";
   import { TextLoader } from "langchain/document_loaders/fs/text";
   import { loadQAChain } from "langchain/chains";
+
+  import {
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    PromptTemplate,
+    SystemMessagePromptTemplate,
+  } from "langchain/prompts";
+
+  import { LLMChain } from "langchain/chains";
+
   dotenv.config();
 
 //   export const run = async () => {
 //      try {
-//       //Instantiante the OpenAI model 
+//       //Instantiante the OpenAI model
 //       //Pass the "temperature" parameter which controls the RANDOMNESS of the model's output. A lower temperature will result in more predictable output, while a higher temperature will result in more random output. The temperature parameter is set between 0 and 1, with 0 being the most predictable and 1 being the most random
-//       const model = new OpenAI({ 
+//       const model = new OpenAI({
 //         modelName:"gpt-3.5-turbo",
 //         temperature: 0.9 });
 //     //   const model = new OpenAI({ openAIApiKey: "sk-NwpX0BjlOmMrJBZIx5urT3BlbkFJ2wVJD8qKhXdyTXyOf",
@@ -31,7 +41,7 @@
 //      }
 //   };
 
-export const run2= async () => {
+export const run2 = async () => {
     try{
          const chat  =  new  OpenAI({ modelName:"gpt-3.5-turbo" ,  temperature: 0.9})
          const  response = await chat.call([
@@ -48,7 +58,7 @@ export const run2= async () => {
 
 
 //loads data from text files
-export const run = async () => {
+export const run33 = async () => {
   const loader = new TextLoader(
     "example.txt"
   );
@@ -57,19 +67,31 @@ export const run = async () => {
 
   try{
     // const chain =  new  OpenAI({ temperature: 0.9})
-    const model = new OpenAI({ 
+    const model = new OpenAI({
                             modelName:"gpt-3.5-turbo" ,
                             });
 
+
+
     console.log("model" , model)
+
+
+    const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+      SystemMessagePromptTemplate.fromTemplate(
+        "You are a helpful assistant that translates"
+      ),
+      HumanMessagePromptTemplate.fromTemplate("{text}"),
+    ]);
+
     // question and answer chain
    const chain = loadQAChain(model);
    console.log("chain", chain)
 //     //console.log(chain)
-//     const res = await chain.call({
-//       input_documents: docs,
-//       question: "ขายที่ไหน",
-//     });
+    const res = await chain.call({
+      input_documents: docs,
+      //question: "ขายที่ไหน",
+      prompt: chatPrompt,
+    });
 
 //     console.log({ res });
  }catch(ex){
@@ -81,7 +103,80 @@ export const run = async () => {
 
 };
 
+export const run = async  () => {
 
+  // We can also construct an LLMChain from a ChatPromptTemplate and a chat model.
+  const chat = new ChatOpenAI({  modelName:"gpt-3.5-turbo" , temperature: 0 });
+  const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+    SystemMessagePromptTemplate.fromTemplate(
+      "คุณคือพนักงานขาย ที่เชี่ยวชาญการขายสินค้า ตอบคำถามอย่างสุภาพ ตอบคำถามลงท้ายด้วยค่ะเสมอ เชียวชาญด้านการใช้ภาษาไทย ทำการเรียบเรียงข้อความใหม่ให้สุภาพและเข้าใจง่ายสำหรับลูกค้า ถ้ามีการสัั่งซื้อ ให้ส่ง url ของ  website"
+     //"คุณคือผู้เชียวชาญด้านการใช้ภาษาไทย ทำการเรียบเรียงข้อความใหม่ให้สุภาพและเข้าใจง่ายสำหรับลูกค้า"
+     ),
+    HumanMessagePromptTemplate.fromTemplate("{question}"),
+  ]);
+
+
+  const loader = new TextLoader(
+    "example.txt"
+  );
+  const docs = await loader.load();
+
+
+  const chainB = new LLMChain({
+    llm: chat,
+    prompt:chatPrompt
+  });
+
+
+
+
+
+  // console.log("chainB", chainB)
+
+  const model = new ChatOpenAI({
+    modelName:"gpt-3.5-turbo" ,
+    temperature: 0.7,
+    });
+
+    // console.log("model" , model)
+
+  const chain = loadQAChain(model);
+
+
+//  console.log("chain", chain)
+
+   const q =  'ขายอะไรครับ'
+  const resB = await chain.call({
+    input_documents: docs,
+    question : q ,
+  });
+
+   console.log( resB.text );
+
+
+  const  ans  = await chainB.call({
+    question :  q + resB.text
+  })
+  console.log(ans)
+
+
+
+//   try{
+//     const chat2 =  new  OpenAI({ modelName:"gpt-3.5-turbo" ,  temperature: 0.9})
+//     const  response = await chat2.call([
+//        new SystemChatMessage(
+//            "คุณคือนักแปรภาษาไทยเป็นอังกฤษ"
+//        ),
+//        new HumanChatMessage("ดี")
+//     ])
+//     console.log(response)
+// }catch(ex){
+//    console.log(ex)
+// }
+
+
+
+}
 
 
 
